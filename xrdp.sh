246eb4f5cc36fa8e6b77b34ed33c7bfe7254f538
Rabyte Build <115883793+rabytebuild@users.noreply.github.com>
2024-05-01 13:12:39 +0100

    Create xrdp.sh

---
+++ xrdp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt-get update -y
+
+# Install XRDP
+sudo apt-get install xrdp -y
+
+# Adding User to xrdp Group
+sudo adduser rhsalisu xrdp
+
+# Start the XRDP service
+sudo service xrdp start
+
+# Enable XRDP to start on boot
+sudo update-rc.d xrdp enable
+
+echo "XRDP installed and started successfully."
