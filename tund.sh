e204e88460d15a14e39dcc6b47248a2b22d6559d
Rabiu Hadi Salisu <94430863+rabiuhadisalisu@users.noreply.github.com>
2024-05-13 03:02:44 +0100

    Update tund.sh

--- tund.sh
+++ tund.sh
@@ -6,14 +6,11 @@ wget https://localtonet.com/download/localtonet-linux-x64.zip
 
 # Step 2: Extract the downloaded file
 echo "Extracting Localtonet..."
-unzip localtonet-linux-x64.zip
-
-# Step 3: Grant permissions
-echo "Granting permissions to Localtonet executable..."
-chmod 777 ./localtonet
+sudo unzip localtonet-linux-x64.zip -d /usr/local/bin
 
 # Step 4: Set up Localtonet with authentication token
 echo "Setting up Localtonet with authentication token..."
-nohup ./localtonet authtoken AzqKXOBuEPJGhadsN2vF9HjfDikntxUWL &> ngrok_out.txt &
+nohup localtonet authtoken AzqKXOBuEPJGhadsN2vF9HjfDikntxUWL &> localtonet.txt &
+cat localtonet.txt
 
 echo "Localtonet setup completed."
